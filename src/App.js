0dc90b8cad572d14a3c59f67a7de2566d9a9a93f
Fachry <fachrykusuma@gmail.com>
2025-01-12 16:43:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Card from "./Card";
 import "./styles.css";
+import React, { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    document.title = "To: Astrid Cintya Ramadhani";
+  }, []);
+
   return (
     <div className="App">
       <Card />
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,28 +1,44 @@
 export default () => (
   <div className="birthdayCard">
     <div className="cardFront">
-      <h3 className="happy">Astrid Cintya Ramadhani</h3>
+      <h3 className="happy">January 13th, 2025</h3>
       <div className="balloons">
         <div className="balloonOne" />
+        <div className="balloonOne" />
         <div className="balloonTwo" />
+        <div className="balloonThree" />
         <div className="balloonThree" />
         <div className="balloonFour" />
       </div>
     </div>
     <div className="cardInside">
-      <h3 className="back">HAPPY BIRTHDAY!!!</h3>
+      <h3 className="back">Happy Birthday!!!</h3>
       <p>My Dearest Maluf,</p>
       <p>
-        Happy Birthday to the love of my life! 🎉 <br />
-        On your special day, I want to remind you how incredibly lucky I am to
-        have you by my side. You light up my world since the day we met, and
-        every moment with you is a gift I cherish. As we step closer to forever
-        together, I can't help but feel excited for all the incredible memories
-        yet to come. Thank you for being the person you are — you make my life
-        richer and fuller just by being in it. Here's to celebrating you today
-        and always. I love you more than words can express.
+        Happy Birthday to the love of my life! 🎂🎉 <br />
+        On your special day, I want to celebrate the amazing woman you are -
+        inside and out. You are my heart, my partner, and the one who makes my
+        life complete. I can't imagine a future without you by my side, and I am
+        so grateful to share this journey with you. <br className="mt"/>
+        As we step closer to forever together, I can't help but feel excited for
+        all the incredible memories yet to come. Thank you for being the person
+        you are, you make my life richer and fuller just by being in it. Wishing
+        you a day as amazing as you are, filled with love, laughter, and
+        everything that makes you smile.
+        <br />I also want to take this moment to say sorry. I know there have
+        been times when I haven't put in the effort I should have, and for that,
+        I am truly sorry. You deserve so much more than I've given, and I want
+        you to know that I am committed to doing better every day. You deserve
+        to feel loved, appreciated, and valued - always.
+        <br />
+        <br />I love you more than words can express.
       </p>
-      <p className="name">Forever yours, F💖</p>
+      <img className="cardImg" src={require("./img2.png")} />
+      <p className="name">
+        Forever yours,
+        <br />
+        F💖
+      </p>
     </div>
   </div>
 );
